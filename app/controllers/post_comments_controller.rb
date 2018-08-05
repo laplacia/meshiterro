3ee8ce71dcdc216992a75656fb49095623bca7e9
永井匠 <iageas7@gmail.com>
2018-08-05 14:39:01 +0900

    Start 5-7

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,16 @@
+class PostCommentsController < ApplicationController
+  def create
+    post_image = PostImage.find(params[:post_image_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.post_image_id = post_image.id
+    comment.save
+    redirect_to post_image_path(post_image)
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:user_id,
+              :post_image_id,
+              :comment)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page])
+  end
+  def edit
+    @user = @User.find(params[:id])
+  end
+  def update
+    @user = @User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
