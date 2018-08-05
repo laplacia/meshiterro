d2510f6b4e65c941f4446262e215ab6ee10dc51d
永井匠 <iageas7@gmail.com>
2018-08-05 11:11:34 +0900

    Start 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show]do resource :post_comments, only: [:create, :destroy]
     resource :favorites,only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
