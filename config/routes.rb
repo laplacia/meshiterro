12bf1f8af01733c4d8dfebdad69c475ac9c0c97a
永井匠 <iageas7@gmail.com>
2018-08-05 12:09:00 +0900

    Finish 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resource :favorites,only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
